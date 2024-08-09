4439aa6a64740b29627e2c987e316f1ba8a9c3d1
mr-vz <vladimirzaharov94@gmail.com>
2024-08-10 00:32:57 +0700

    weather

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import './App.css';
+import Weather from "./Weather";
+
+function App() {
+
+    const [city, setCity] = useState<string>('')
+    //console.log(city)
+    const [error, setError] = useState<string | null>(null)
+
+    const [weather, setWeather] = useState<{ temp: number, description: string } | null>(null);
+
+    const fetchWeather = () => {
+        //const city = 'London'
+        const apiKey = '90ced5ac8945319329c4cc56ca1e6939'
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+            .then(response => response.json())
+            .then(json => {
+                if (json.cod === "404") {
+                    setError('City not found');
+                    setWeather(null);
+                } else {
+                    setWeather({
+                        temp: json.main.temp,
+                        description: json.weather[0].description
+                    });
+                    setError(null);
+                }
+            })
+            .catch(error => {
+                console.error('Ошибка:', error);
+                setError('An error occurred');
+                setWeather(null);
+            });
+    }
+
+    return (
+        <div className="App">
+            <h1>Weather App</h1>
+            <input type="text" value={city} onChange={(e) => setCity(e.currentTarget.value)}/>
+            <button onClick={fetchWeather}>Get weather</button>
+            {error && <div style={{color: 'red'}}>{error}</div>}
+            {weather&&<Weather temp={weather.temp} description={weather.description}/>}
+        </div>
+    );
+}
+
+export default App;
